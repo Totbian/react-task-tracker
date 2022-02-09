101d70a41de5d26b6cc88d67ee406bd89b510df2
Benedek Toth <benedek.toth.uk@gmail.com>
2022-02-09 12:57:32 +0000

    Updates

---
+++ precache-manifest.e763e71dc5a675b5ee4521e79a3e64af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8aa93612b2e5fb4d9ce147cdc3e1168",
+    "url": "/react-task-tracker/index.html"
+  },
+  {
+    "revision": "0286eedf03865f181b5b",
+    "url": "/react-task-tracker/static/css/main.10f4e31e.chunk.css"
+  },
+  {
+    "revision": "fa05702569c6273f03b7",
+    "url": "/react-task-tracker/static/js/2.72d58efe.chunk.js"
+  },
+  {
+    "revision": "c3119482f6b2bf53b2f0",
+    "url": "/react-task-tracker/static/js/3.6c7db885.chunk.js"
+  },
+  {
+    "revision": "0286eedf03865f181b5b",
+    "url": "/react-task-tracker/static/js/main.8fd0cff8.chunk.js"
+  },
+  {
+    "revision": "83c5c4c2e03703964b44",
+    "url": "/react-task-tracker/static/js/runtime~main.9f53d0e3.js"
+  }
+]);
